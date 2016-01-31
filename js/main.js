00400b666f727b708f7ed1e3a77c8475a2f878e8
David Friesen <davidfriesen@David-Friesens-MacBook.local>
2016-01-31 15:40:33 -0500

    set up sidebar and collaspability. next will be to add form elements within it, notably character selection stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+var mapObj = document.querySelector('.map');
+var sbarObj = document.querySelector('.sidebar');
 
+document.querySelector('.toggleSidebar').onclick = function(evt) {
+	if (sbarObj.style.visibility === "visible") {
+		sbarObj.style.visibility = "hidden";
+		bars = document.getElementsByClassName('bar');
+		for (var i = 0; i < 3; i++) {
+			bars[i].style.backgroundColor = "#DCDCDC";
+			bars[i].querySelector('div').style.backgroundColor = "#232323";
+		}
+	} else {
+		sbarObj.style.visibility = "visible";
+		bars = document.getElementsByClassName('bar');
+		for (var i = 0; i < 3; i++) {
+			bars[i].style.backgroundColor = "#232323";
+			bars[i].querySelector('div').style.backgroundColor = "#DCDCDC";
+		}
+	}
+};
