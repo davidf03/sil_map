e978a02bd8e24686da2a9af8a7c1373a3babae96
David Friesen <davidfriesen@David-Friesens-MacBook.local>
2016-01-31 17:58:39 -0500

    sidebar remains fixed, but its components still need to be styled.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-var mapObj = document.querySelector('.map');
-var sbarObj = document.querySelector('.sidebar');
+document.onready = function(evt) {
 
-document.querySelector('.toggleSidebar').onclick = function(evt) {
-	if (sbarObj.style.visibility === "visible") {
-		sbarObj.style.visibility = "hidden";
-		bars = document.getElementsByClassName('bar');
-		for (var i = 0; i < 3; i++) {
-			bars[i].style.backgroundColor = "#DCDCDC";
-			bars[i].querySelector('div').style.backgroundColor = "#232323";
+	var mapObj = document.querySelector('.map');
+	var sbarObj = document.querySelector('.sidebar');
+
+	// toggle sidebar visibility on/off
+	document.querySelector('.toggleSidebar').onclick = function(evt) {
+		if (sbarObj.style.visibility === "visible") {
+			sbarObj.style.visibility = "hidden";
+			var bars = document.getElementsByClassName('bar');
+			for (var i = 0; i < 3; i++) {
+				bars[i].style.backgroundColor = "#DCDCDC";
+				bars[i].querySelector('div').style.backgroundColor = "#232323";
+			}
+		} else {
+			sbarObj.style.visibility = "visible";
+			var bars = document.getElementsByClassName('bar');
+			for (var i = 0; i < 3; i++) {
+				bars[i].style.backgroundColor = "#232323";
+				bars[i].querySelector('div').style.backgroundColor = "#DCDCDC";
+			}
 		}
-	} else {
-		sbarObj.style.visibility = "visible";
-		bars = document.getElementsByClassName('bar');
-		for (var i = 0; i < 3; i++) {
-			bars[i].style.backgroundColor = "#232323";
-			bars[i].querySelector('div').style.backgroundColor = "#DCDCDC";
-		}
+	};
+
+	var addEvent = function(object, type, callback) {
+	    if (object == null || typeof(object) == 'undefined') return;
+	    if (object.addEventListener) {
+	        object.addEventListener(type, callback, false);
+	    } else if (object.attachEvent) {
+	        object.attachEvent("on" + type, callback);
+	    } else {
+	        object["on"+type] = callback;
+	    }
+	};
+
+	addEvent(window, "resize", fixSidebar);
+
+	function fixSidebar() {
+		sbarObj.style.right = window.innerWidth - 280 + "px";
 	}
+	
+	fixSidebar();
 };
